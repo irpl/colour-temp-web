2f6ff168e61bdd00ab78ddec4dfb4a8f718b599a
Phillip Logan <philo3194@gmail.com>
2022-02-27 01:00:14 +0000

    trying

--- api/app.py
+++ api/app.py
@@ -12,17 +12,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGO_CONNECT_STRING")
 mongo = PyMongo(app)
 
-@app.route('/api/temp/<mac>', methods=["PATCH"])
+@app.route('/api/temp', methods=["PATCH"])
 def patch(mac):
   temp = request.json["temp"]
-  res = mongo.db.temp.find_one_and_update({"mac": mac},{"$set": {"temp": temp}}, return_document=ReturnDocument.AFTER)
+  res = mongo.db.temp.find_one_and_update({"mac": os.getenv("MAC_ADDRESS")},{"$set": {"temp": temp}}, return_document=ReturnDocument.AFTER)
   if res == None:
     return 404
   return loads(dumps(res))
 
-@app.route('/api/temp/<mac>', methods=["GET"])
+@app.route('/api/temp', methods=["GET"])
 def get(mac):
-  res = mongo.db.temp.find_one({"mac": mac})
+  res = mongo.db.temp.find_one({"mac": os.getenv("MAC_ADDRESS")})
 
   if res == None:
     return 404
